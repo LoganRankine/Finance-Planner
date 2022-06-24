9690dae1b8011ba56ecb7f34f2318744cbbc8af1
LoganRankine <loganrankine@live.co.uk>
2022-06-24 19:25:55 +0100

    Added UserBudgetInfo Class

---
+++ Finance-Planner/UserBudgetInfo.cs
@@ -0,0 +1,22 @@
+﻿using Foundation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+
+//add sqlite functionality
+using SQLite;
+
+namespace Finance_Planner
+{
+    class UserBudgetInfo
+    {
+        [PrimaryKey, AutoIncrement]
+        public int m_ID { get; set; }
+        [MaxLength(50)]
+        public string m_reason { get; set; }
+        public string m_date { get; set; }
+        public int m_cost { get; set; }
+    }
+}
