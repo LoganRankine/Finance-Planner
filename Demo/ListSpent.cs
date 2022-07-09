01dcf62d6bd60aad2f80738c2f8e5482c58d30fa
Logan Rankine <loganrankine@Logans-Mac.local>
2022-07-09 17:27:58 +0100

    new view controller and table class

---
+++ Demo/ListSpent.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UIKit;
+using Foundation;
+using System.Collections.Generic;
+namespace Demo
+{
+    public class ListSpent : UITableViewSource
+    {
+        List<BudgetInfo> BudgetInfo = new List<BudgetInfo>();
+        string cellIdentifer = "BudgetCell";
+        public ListSpent(List<BudgetInfo> budgetInfo)
+        {
+            BudgetInfo = budgetInfo;
+        }
+
+        public override UITableViewCell GetCell(UITableView tableView, NSIndexPath indexPath)
+        {
+            UITableViewCell cell = tableView.DequeueReusableCell(cellIdentifer);
+            if (cell == null)
+            {
+                cell = new UITableViewCell(UITableViewCellStyle.Subtitle, cellIdentifer);
+            }
+            //cell.TextLabel.Text = tableItems[indexPath.Row];
+            cell.TextLabel.Text = BudgetInfo[indexPath.Row].m_Reason;
+            cell.DetailTextLabel.Text = BudgetInfo[indexPath.Row].m_Date;
+            
+            return cell;
+        }
+
+        public override nint RowsInSection(UITableView tableview, nint section)
+        {
+            return BudgetInfo.Count;
+        }
+    }
+}
+
