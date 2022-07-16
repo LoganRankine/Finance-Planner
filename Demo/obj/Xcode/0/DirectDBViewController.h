d6ff504a43afc179ce1d89e0e3593b53a039e70b
Logan Rankine <loganrankine@Logans-Mac.local>
2022-07-16 02:10:57 +0100

    Added slider that asks if you have direct debits so you can add them

---
+++ Demo/obj/Xcode/0/DirectDBViewController.h
@@ -0,0 +1,16 @@
+//
+//  DirectDBViewController.h
+//  Demo
+//
+//  Created by Logan Rankine on 16/07/2022.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DirectDBViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo/obj/Xcode/0/DirectDBViewController.m
@@ -0,0 +1,31 @@
+//
+//  DirectDBViewController.m
+//  Demo
+//
+//  Created by Logan Rankine on 16/07/2022.
+//
+
+#import "DirectDBViewController.h"
+
+@interface DirectDBViewController ()
+
+@end
+
+@implementation DirectDBViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
